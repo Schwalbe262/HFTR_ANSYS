ba9c4e8a0a40a35950d2cd0fdf37cc41335088e1
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2021-07-12 14:36:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,132 @@
 import ScriptEnv
 ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
 oDesktop.RestoreWindow()
-oDesktop.OpenProject("D:/transformer_2021/TR_2021_07_09.aedt")
-oProject = oDesktop.SetActiveProject("TR_2021_07_09")
-oDesign = oProject.SetActiveDesign("HFTR")
+
+# Open aedt file
+oDesktop.OpenProject("D:/transformer_2021/ML_v1.aedt")
+
+# Make project
+oProject = oDesktop.SetActiveProject("ML_v1")
+oProject.InsertDesign("HFSS", "HFSSDesign1", "DrivenModal", "")
+oDesign = oProject.SetActiveDesign("HFSSDesign1")
+oDesign.RenameDesignInstance("HFSSDesign1", "HFSS_name") # project name 변경
+
+# Make setup
+oModule = oDesign.GetModule("AnalysisSetup")
+oModule.InsertSetup("HfssDriven", 
+	[
+		"NAME:Setup1",
+		"AdaptMultipleFreqs:="	, False,
+		"Frequency:="		, "40kHz",
+		"MaxDeltaE:="		, 0.1,
+		"MaximumPasses:="	, 2,
+		"MinimumPasses:="	, 1,
+		"MinimumConvergedPasses:=", 1,
+		"PercentRefinement:="	, 30,
+		"IsEnabled:="		, True,
+		[
+			"NAME:MeshLink",
+			"ImportMesh:="		, False
+		],
+		"BasisOrder:="		, 0,
+		"DoLambdaRefine:="	, False,
+		"DoMaterialLambda:="	, True,
+		"SetLambdaTarget:="	, False,
+		"Target:="		, 0.3333,
+		"UseMaxTetIncrease:="	, False,
+		"UseDomains:="		, False,
+		"UseIterativeSolver:="	, False,
+		"SaveRadFieldsOnly:="	, False,
+		"SaveAnyFields:="	, True,
+		"IESolverType:="	, "Auto",
+		"LambdaTargetForIESolver:=", 0.15,
+		"UseDefaultLambdaTgtForIESolver:=", True,
+		"IE Solver Accuracy:="	, "Balanced"
+	])
+
+# Set variable
+oDesign.ChangeProperty(
+	[
+		"NAME:AllTabs",
+		[
+			"NAME:LocalVariableTab",
+			[
+				"NAME:PropServers", 
+				"LocalVariables"
+			],
+			[
+				"NAME:NewProps",
+				[
+					"NAME:air",
+					"PropType:="		, "VariableProp",
+					"UserDef:="		, True,
+					"Value:="		, "300mm",
+					"NAME:l1",
+					"PropType:="		, "VariableProp",
+					"UserDef:="		, True,
+					"Value:="		, "10mm",
+					"NAME:l2",
+					"PropType:="		, "VariableProp",
+					"UserDef:="		, True,
+					"Value:="		, "50mm",
+					"NAME:h1",
+					"PropType:="		, "VariableProp",
+					"UserDef:="		, True,
+					"Value:="		, "50mm",
+					"NAME:w1",
+					"PropType:="		, "VariableProp",
+					"UserDef:="		, True,
+					"Value:="		, "30mm",
+					
+				]
+			]
+		]
+	])
+
+# Make boundary
 oEditor = oDesign.SetActiveEditor("3D Modeler")
 oEditor.CreateBox(
 	[
 		"NAME:BoxParameters",
-		"XPosition:="		, "-width/2",
+		"XPosition:="		, "-air",
+		"YPosition:="		, "-air",
+		"ZPosition:="		, "-air",
+		"XSize:="		, "2*air",
+		"YSize:="		, "2*air",
+		"ZSize:="		, "2*air"
+	], 
+	[
+		"NAME:Attributes",
+		"Name:="		, "Box1",
+		"Flags:="		, "",
+		"Color:="		, "(143 175 143)",
+		"Transparency:="	, 0,
+		"PartCoordinateSystem:=", "Global",
+		"UDMId:="		, "",
+		"MaterialValue:="	, "\"vacuum\"",
+		"SurfaceMaterialValue:=", "\"\"",
+		"SolveInside:="		, True,
+		"IsMaterialEditable:="	, True,
+		"UseMaterialAppearance:=", False,
+		"IsLightweight:="	, False
+	])
+oModule = oDesign.GetModule("BoundarySetup")
+oModule.AssignRadiation(
+	[
+		"NAME:Rad1",
+		"Objects:="		, ["Air"],
+		"IsFssReference:="	, False,
+		"IsForPML:="		, False
+	])
+
+# Make core
+oEditor.CreateBox(
+	[
+		"NAME:BoxParameters",
+		"XPosition:="		, "-w1/2",
 		"YPosition:="		, "-2*l1-l2",
 		"ZPosition:="		, "-l3-h1/2",
-		"XSize:="		, "width",
+		"XSize:="		, "w1",
 		"YSize:="		, "4*l1+2*l2",
 		"ZSize:="		, "2*l3+h1"
 	], 
@@ -37,10 +152,10 @@ oEditor.CreateBox(
 oEditor.CreateBox(
 	[
 		"NAME:BoxParameters",
-		"XPosition:="		, "-width/2",
+		"XPosition:="		, "-w1/2",
 		"YPosition:="		, "-l1",
 		"ZPosition:="		, "-h1/2",
-		"XSize:="		, "width",
+		"XSize:="		, "w1",
 		"YSize:="		, "-l2",
 		"ZSize:="		, "h1"
 	], 
@@ -62,10 +177,10 @@ oEditor.CreateBox(
 oEditor.CreateBox(
 	[
 		"NAME:BoxParameters",
-		"XPosition:="		, "-width/2",
+		"XPosition:="		, "-w1/2",
 		"YPosition:="		, "l1",
 		"ZPosition:="		, "-h1/2",
-		"XSize:="		, "width",
+		"XSize:="		, "w1",
 		"YSize:="		, "l2",
 		"ZSize:="		, "h1"
 	], 
